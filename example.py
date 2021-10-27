e71f6f32966a0e7bcb171e1a4783df3337d94625
Umut Ediz <umtedz@gmail.com>
2021-10-27 22:18:56 +0300

    example updated as baudrate config

--- example.py
+++ example.py
@@ -1,36 +1,18 @@
 from actuator import *
 import copy
 
-m = Master(4096, "/dev/ttyACM0")
+m = Master(4096, "/dev/cu.usbmodem103", baudrate=115200)
 
-m.AutoScan()
+print(m.AutoScan())
 
 #Print last reply timestamps of actuators on the bus
 for actuator in m.ActList:
-	m.send(actuator.Ping())
+	m.send(m.Actuators[actuator].Ping())
 	m.pass2buffer(m.receive())
 	m.findPackage()
-	print(m.Timestamps[actuator.Configuration.data.devID.data])
-
-#Set all actuators to position control mode
+	print(m.Timestamps[m.Actuators[actuator].Configuration.data.devID.data])
 
 for actuator in m.ActList:
 	act = Actuator(255) #temporary actuator object
-	act.Configuration.data.operationMode.data = OperationModes.PositionControl
-	m.send(actuator.Write(act, [Parameters.operationMode]))
-
-for actuator in m.ActList:
-	act = copy.deepcopy(actuator)
-	act.PositionControl.data.proportionalGain.data = 0.1
-	act.PositionControl.data.integralGain.data = 0
-	act.PositionControl.data.derivativeGain.data = 0.015
-	act.PositionControl.data.feedForward.data = 90
-	m.send(actuator.Write(act)) #Alternative way to write
-
-act = Actuator(Actuator.BATCH_ID) #Apply to all actuators
-
-#Continious stepping motion
-while True:
-	act.PositionControl.data.setpoint.data += 100
-	m.send(actuator.Write(act, [Parameters.posSetpoint]))
-	time.sleep(1)
+	act.Configuration.data.baudRate.data = 57600
+	m.send(m.Actuators[actuator].Write(act, [Parameters.baudrate]))
