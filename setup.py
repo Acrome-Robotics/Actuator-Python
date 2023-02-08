f3bf80fad224216b91012f872ea81da3df20b59e
Furkan Kırlangıç <furkan.kirlangic@std.yildiz.edu.tr>
2023-02-08 14:37:24 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -8,12 +8,12 @@ setuptools.setup(
     version="0.0.1",
     author="Furkan Kırlangıç",
     author_email="furkankirlangic@acrome.net",
-    description="Python library for interfacing with Acrome Smart Motor Driver (SMD) products.",
+    description="Python library for interfacing with Acrome Smart Motor Drivers (SMD) products.",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/acrome-robotics/acrome_smd_python_lib",
+    url="https://github.com/Acrome-Robotics/acrome-smd-python-lib",
     project_urls={
-        "Bug Tracker": "https://github.com/acrome-robotics/acrome_smd_python_lib/issues",
+        "Bug Tracker": "https://github.com/Acrome-robotics/acrome_smd_python_lib/issues",
         },
     classifiers=[
         "Programming Language :: Python :: 3",
@@ -21,6 +21,6 @@ setuptools.setup(
         "Operating System :: OS Independent",
     ],
     packages=setuptools.find_packages(exclude=['tests', 'test']),
-    install_requires=["pyserial", "crccheck", "requests", "packaging"],
+    install_requires=["pyserial", "stm32loader", "crccheck", "requests", "packaging"],
     python_requires=">=3.6"
 )
