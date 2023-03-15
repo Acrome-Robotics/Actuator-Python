4701c0cf9319174a69b00da0e43d04504ae2ea65
Furkan Kirlangic <furkan.kirlangic@std.yildiz.edu.tr>
2023-03-15 18:14:15 +0300

    Fix wrong assertion type and add error clear

--- tests/test_actuator.py
+++ tests/test_actuator.py
@@ -11,43 +11,35 @@ class TestActuator(unittest.TestCase):
     def tearDown(self):
         pass
 
-    def test_populate_header(self):
-        header = bytes([0x55, 0x00, 0x15, 0x0, 0x03])
-
-        ret = self.dev.__populate_header()
-        self.assertEqual(ret, header)
-        pass
-
-    def test_calculate_crc(self):
-        data = bytes([0x55, 0x00, 0x09, 0x00, 0x00])
-        crc = bytes([0x7F, 0x9A, 0xEC, 0xA4])
-
-        ret = self.dev.__calculate_crc(data)
-        self.assertEqual(ret, crc)
-
     def test_ping(self):
         pkg_ping = bytes([0x55, 0x00, 0x09, 0x00, 0x00, 0x7F, 0x9A, 0xEC, 0xA4])
 
         ret = self.dev.Ping()
-        self.assertListEqual(ret, pkg_ping)
+        self.assertEqual(ret, pkg_ping)
 
     def test_reboot(self):
         pkg_reboot = bytes([0x55, 0x00, 0x09, 0x05, 0x00, 0xA0, 0x7A, 0xD1, 0x33])
 
         ret = self.dev.Reboot()
-        self.assertListEqual(ret, pkg_reboot)
+        self.assertEqual(ret, pkg_reboot)
 
     def test_factory_reset(self):
         pkg_rst = bytes([0x55, 0x00, 0x09, 0x17, 0x00, 0x16, 0x7F, 0x72, 0x83])
 
         ret = self.dev.FactoryReset()
-        self.assertListEqual(ret, pkg_rst)
+        self.assertEqual(ret, pkg_rst)
 
-    def test_rom_write(self):
-        pkg_rom_wr = bytes([0x55, 0x00, 0x09, 0x03, 0x00, 0xAC, 0xC5, 0x70, 0xD6])
+    def test_error_clear(self):
+        pkg_err_clr = bytes([0x55, 0x00, 0x09, 0x18, 0x00, 0x3E, 0x42, 0xF5, 0x3E])
+
+        ret = self.dev.ErrorClear()
+        self.assertEqual(ret, pkg_err_clr)
+
+    def test_eeprom_write(self):
+        pkg_eeprom_wr = bytes([0x55, 0x00, 0x09, 0x03, 0x00, 0xAC, 0xC5, 0x70, 0xD6])
 
         ret = self.dev.ROMWrite()
-        self.assertListEqual(ret, pkg_rom_wr)
+        self.assertEqual(ret, pkg_eeprom_wr)
 
     @unittest.skip
     def test_write(self):
