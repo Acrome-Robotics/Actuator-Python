2f5a4e5cebf54593d6079335e865c51635c8f1ef
Furkan Kirlangic <furkan.kirlangic@std.yildiz.edu.tr>
2023-07-17 13:50:30 +0300

    Update commands and extend index list

--- smd/_internals.py
+++ smd/_internals.py
@@ -8,14 +8,14 @@ class FamilyType():  # Specify Red, Blue and Green types
 
 class Commands(enum.IntEnum):
     PING = 0x00,
-    READ = 0x01,
-    WRITE = 0x02,
+    WRITE = 0x01,
+    READ = 0x02,
     REBOOT = 0x05,
     EEPROM_WRITE = 0x03,
     BL_JUMP = 0x30,
-    __SYNC_WRITE = -1
-    __BULK_WRITE = -1
-    __BULK_READ = -1
+    SYNC_WRITE = 0x40 | 0x01
+    BULK_WRITE = 0x20 | 0x01
+    BULK_READ = 0x20 | 0x02
     ACK = 0x80,
     __WRITE_ACK = 1
     __EEPROM_WRITE_ACK = -1
@@ -30,6 +30,52 @@ Index = enum.IntEnum('Index', [
     'Status',
     'Baudrate',
     'OperationMode',
+    'TorqueEnable',
+    'AutotunerEnable',
+    'AutotuneMethod',
+    'MotorCPR',
+    'MotorRPM',
+    'PWMFrequency',
+    'SetDutyCycle',
+    'MinimumPosition',
+    'MaximumPosition',
+    'TorqueLimit',
+    'VelocityLimit',
+    'PositionFF',
+    'VelocityFF',
+    'TorqueFF',
+    'PosScalerGain',
+    'PosPGain',
+    'PosIGain',
+    'PosDGain',
+    'VelScalerGain',
+    'VelPGain',
+    'VelIGain',
+    'VelDGain',
+    'TorqueScalerGain',
+    'TorquePGain',
+    'TorqueIGain',
+    'TorqueDGain',
+    'SetPosition',
+    'SetTorque',
+    'SetVelocity',
+    'BuzzerEnable',
+    'PresentPosition',
+    'PresentVelocity',
+    'MotorCurrent',
+    'InternalRoll',
+    'InternalPitch',
+    'ExternalRoll',
+    'ExternalPitch',
+    'AmbientLight',
+    'IsButtonPressed',
+    'Distance',
+    'JoystickX',
+    'JoystickY',
+    'JoystickButton',
+    'QtrR',
+    'QtrM',
+    'QtrL',
     'CRCValue',
     ], start=0)
 
