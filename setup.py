538dfaf1ec3a53f1ba9345b2226f7edb26c3b71f
Berat Computer <berat.dogan@std.yildiz.edu.tr>
2024-10-11 09:47:37 +0300

    version update

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="acrome-smd",
-    version="2.0.4",
+    version="2.0.5",
     author="Furkan Kırlangıç",
     author_email="furkankirlangic@acrome.net",
     description="Python library for interfacing with Acrome Smart Motion Devices (SMD) products.",
